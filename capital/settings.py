f42269b7e8b1cf96786c430e957a20b66b5f1bf7
emekanjoags <68468197+emekanjoags@users.noreply.github.com>
2020-09-07 13:38:06 +0100

    Update settings.py

--- capital/settings.py
+++ capital/settings.py
@@ -130,10 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 
 #smtp configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'emekanjoagwuani@gmail.com'
-EMAIL_HOST_PASSWORD = 'kingpower'
+
 
